e822e199773b04ab0fa4d72c02ba150367803809
Kyle Close <k.james.close@gmail.com>
2024-02-29 21:15:37 -0800

    adding intervals

--- src/pomodoro/interval-markers/IntervalMarkers.tsx
+++ src/pomodoro/interval-markers/IntervalMarkers.tsx
@@ -1,3 +1,19 @@
-export function IntervalMarkers() {
-  return <div>1 2 3 4</div>;
+interface IntervalMarkersProps {
+  count: number;
+  completed: number;
+}
+
+export function IntervalMarkers({ count, completed }: IntervalMarkersProps) {
+  console.log(`count: ${count}, completed: ${completed}`);
+
+  const Markers = () => {
+    let temp = [];
+    for (let i = 0; i < count; i++) {
+      if (i >= completed) temp.push(<div key={i} className='empty-interval-marker'></div>);
+      else temp.push(<div key={i} className='filled-interval-marker'></div>);
+    }
+    return temp;
+  };
+
+  return <div className='interval-container'>{Markers()}</div>;
 }
--- src/pomodoro/usePomodoro.ts
+++ src/pomodoro/usePomodoro.ts
@@ -1,27 +1,36 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export function usePomodoro() {
-  const DEFAULT_START_TIME = 25 * 60;
+  const DEFAULT_START_TIME = 5; //25 * 60;
   let intervalId = useRef<number | null>(null);
   const [timeInSeconds, setTimeInSeconds] = useState(DEFAULT_START_TIME);
   const [isOn, setIsOn] = useState(false);
+  const [intervalCount, setIntervalCount] = useState(3);
+  const [completedInvervals, setCompletedIntervals] = useState(0);
 
-  const start = () => {
+  useEffect(() => {
+    if (timeInSeconds === 0) {
+      reset();
+      setCompletedIntervals((prev) => prev + 1);
+    }
+  }, [timeInSeconds]);
+
+  function start() {
     if (!intervalId.current) {
       intervalId.current = setInterval(() => {
         setTimeInSeconds((prevTimeInSeconds) => prevTimeInSeconds - 1);
       }, 1000);
     }
     setIsOn(true);
-  };
+  }
 
-  const pause = () => {
+  function pause() {
     if (intervalId.current) {
       clearInterval(intervalId.current);
       intervalId.current = null;
       setIsOn(false);
     }
-  };
+  }
 
   const handleStartPauseClick = () => {
     if (intervalId.current) {
@@ -40,16 +49,16 @@ export function usePomodoro() {
     return { minutes, seconds };
   };
 
-  const formatNumberToString = (number: number) => {
+  function formatNumberToString(number: number) {
     if (number < 10) {
       return '0' + number;
     } else return number.toString();
-  };
+  }
 
-  const reset = () => {
+  function reset() {
     pause();
     setTimeInSeconds(DEFAULT_START_TIME);
-  };
+  }
 
   const { minutes, seconds } = formatTime(timeInSeconds);
 
@@ -59,5 +68,7 @@ export function usePomodoro() {
     handleStartPauseClick,
     minutes,
     seconds,
+    intervalCount,
+    completedInvervals,
   };
 }
